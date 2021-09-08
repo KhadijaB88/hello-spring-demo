f944cffc55481971db79787ca25665fbe2fd7279
Khadija Bano <kbano88@gmail.com>
2021-09-07 23:49:41 -0500

    Chapter 10.5 Exercises complete

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -8,33 +8,42 @@ import org.springframework.web.bind.annotation.*;
  */
 @Controller
 @ResponseBody
-@RequestMapping("hello")
+
 public class HelloController {
 
-    //    // Handle request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
+    public static String createMessage(String name, String language){
+        String greeting ="";
+        if (language.equals("English")) {
+            greeting = "Hello";
+        }
+        else if (language.equals("French")) {
+            greeting = "Bonjour";
+        }
+        else if (language.equals("Hindi")) {
+            greeting = "Namaste";
+        }
+        else if (language.equals("Spanish")) {
+            greeting = "Hola";
+        }
+        else if (language.equals("Urdu")) {
+            greeting = "Salam";
+        }
 
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
+        return greeting + " " + name;
     }
 
-    // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+
+    @RequestMapping(value= "hello", method = {RequestMethod.GET, RequestMethod.POST})
+
+    public String helloGreeting(@RequestParam String name, @RequestParam String language) {
+
+        if(name == null) {
+            name = "world";
+        }
+
+       return createMessage(name, language);
     }
 
-    // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
 
     // /hello/form
     @GetMapping("form")
@@ -44,6 +53,13 @@ public class HelloController {
                 "<form action = '/hello' method = 'post'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
+                "<select name='language' id ='language'>" +
+                "<option value = 'English'> English </option>"+
+                "<option value = 'French'> French </option>"+
+                "<option value = 'Hindi'> Hindi </option>"+
+                "<option value = 'Spanish'>Spanish </option>"+
+                "<option value = 'Urdu'> Urdu </option>"+
+                "</select>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
